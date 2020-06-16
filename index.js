61d63177b93cabf0861489448e472f26924d6b91
thank5581 <thank5581@gmail.com>
2020-06-16 11:51:33 -0400

    first lesson

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+    return a+b;
+ }
+ function subtract(a,b){
+    return a-b;
+ }
+ function multiply(a,b){
+    return a*b;
+ }
+ function divide(a,b){
+    return a/b;
+ }
+ function inc(n){
+    n++;
+    return n;
+ }
+ function dec(n){
+    n--;
+    return n;
+ }
+ function makeInt(n) {
+    n = parseInt(n, 10);
+    return n;
+ }
+var number = n
+number++
+console.log(n)
+number--
+--number
+++number
+number += 3
+number -= 2
+number *= 10
+number /= 5
+
+
